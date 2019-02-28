689753159db15b6f57b702557d78477056880115
OskaMathis <oskamathis@gmail.com>
2019-02-28 18:59:46 +0900

    CloudStore で四苦八苦

--- mobile/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
+++ mobile/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
@@ -1,12 +1,104 @@
 package com.oskamathis.watchtracker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.common.collect.ImmutableList
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.math.log
+//import jdk.nashorn.internal.runtime.ECMAException.getException
+//import androidx.test.orchestrator.junit.BundleJUnitUtils.getResult
+import com.google.firebase.firestore.QueryDocumentSnapshot
+//import org.junit.experimental.results.ResultMatchers.isSuccessful
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val taskNames = getTaskNames()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerAdapter(this, taskNames)
+    }
+
+    private fun getTaskNames() : List<String>{
+        val taskNames = mutableListOf<String>()
+        val docRef = db.collection("Tasks")
+//        val docRef = db.collection("Tasks").document("PM業務説明")
+
+        docRef.get().addOnCompleteListener { task->
+//            if (task.isSuccessful) {
+//                val document = task.result ?: return@addOnCompleteListener
+//                if (document.exists()) {
+//                    Log.d("MainActivity", "DocumentSnapshot data: " + document.data)
+//                } else {
+//                    Log.d("MainActivity", "No such document");
+//                }
+//            } else {
+////                Log.d("MainActivity", "get failed with ", task.getException());
+//            }
+            if (task.isSuccessful) {
+                val result = task.result ?: return@addOnCompleteListener
+                for (document in result) {
+                    val taskName = document.data["name"]
+                    Log.d("MainActivity", document.id + " => " + document.data["name"])
+                    taskNames.add(taskName.toString())
+                }
+            } else {
+                Log.d("MainActivity", "Error getting documents: ", task.exception)
+            }
+        }
+        return taskNames
+    }
+
+
+
+
+}
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val textView: TextView = itemView.findViewById(R.id.textView)
+}
+
+class RecyclerAdapter(context: Context, val data: List<String>) : RecyclerView.Adapter<ViewHolder>() {
+    val inflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // ここでViewHolderを作る
+        return ViewHolder(inflater.inflate(R.layout.list_item, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        // データの要素数を返す
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // ViewHolderを通してデータをViewに設定する
+        holder.textView.text = data[position]
     }
 }
+
+data class Tasks(val tasks:List<Task>)
+data class Task(val name:String, val times:List<Time>)
+data class Time(val started_at:Date, val stopped_at:Date)
--- wear/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
+++ wear/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.oskamathis.watchtracker
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.wearable.activity.WearableActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : WearableActivity() {
+    lateinit var startTime: Date
+    private var elapsedTime: Long = 0
+    private var isTracking: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Enables Always-on
+        initText()
+
+        button.setOnClickListener {
+            button.text = if (isTracking) "START" else "END"
+            onButtonClicked()
+            isTracking = !isTracking
+        }
+
         setAmbientEnabled()
+    }
+
+    private fun initText() {
+        button.text = "START"
+        total_time.text = "00:00:00"
+        timer.text = "00:00"
+    }
+
+
+    private fun onButtonClicked() {
+        if (isTracking) {
+            elapsedTime += Date().time - startTime.time
+            total_time.text = milliSecFormatter(elapsedTime)
+            timer.stop()
+        } else {
+            startTime = Date()
+            timer.base = SystemClock.elapsedRealtime()
+            timer.start()
+        }
+
+    }
+
+    private fun milliSecFormatter(ms: Long): String {
+        val timeDifference = ms / 1000
+        val h = (timeDifference / (3600)).toInt()
+        val m = ((timeDifference - (h * 3600)) / 60).toInt()
+        val s = (timeDifference - (h * 3600) - m * 60).toInt()
+
+        return String.format("%02d:%02d:%02d", h, m, s)
     }
 }
