2bf8aa0c053d1da02974b92969fcf1b28b784cf4
OskaMathis <oskamathis@gmail.com>
2019-03-01 18:29:06 +0900

    [WIP]SwipeRefresh対応

--- mobile/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
+++ mobile/src/main/java/com/oskamathis/watchtracker/MainActivity.kt
@@ -23,12 +23,13 @@ import kotlin.math.log
 //import jdk.nashorn.internal.runtime.ECMAException.getException
 //import androidx.test.orchestrator.junit.BundleJUnitUtils.getResult
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
 
 //import org.junit.experimental.results.ResultMatchers.isSuccessful
 
 
 class MainActivity : AppCompatActivity() {
-
     private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,23 +38,61 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(itemDecoration)
-        getTaskNames()
+        reloadTasks()
+        showTasks()
+//        getTimes()
     }
 
-    private fun getTaskNames() {
-        val taskNames = mutableListOf<String>()
-        val docRef = db.collection("tasks")
-        docRef.get().addOnCompleteListener { task ->
+    private fun reloadTasks() {
+        db.collection("tasks").addSnapshotListener { value, e ->
+            if (e != null) {
+                Log.w("MainActivity", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+            val tasks = mutableListOf<String>()
+
+            if (value != null) {
+                for (doc in value) {
+                    if (doc.get("name") != null) {
+                        tasks.add(doc.getString("name")!!)
+                    }
+                }
+            }
+            Log.d("MainActivity", "Current tasks: $tasks")
+        }
+    }
+
+    private fun showTasks() {
+        db.collection("tasks").get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val result = task.result ?: return@addOnCompleteListener
+                val taskNames = mutableListOf<String>()
                 for (document in result) {
                     val taskName = document.data["name"].toString()
-                    Log.d("MainActivity", document.id + " => " + taskName)
+                    Log.d("MainActivity", "${document.data}")
                     taskNames.add(taskName)
                     recyclerView.adapter = RecyclerAdapter(this, taskNames)
                 }
             } else {
                 Log.d("MainActivity", "Error getting documents: ", task.exception)
+            }
+        }
+    }
+
+    private fun getTimes() {
+        db.collection("tasks").get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val result = it.result ?: return@addOnCompleteListener
+                for (task in result) {
+                    db.collection("tasks").document(task.id).collection("times").get().addOnCompleteListener { it2 ->
+                        val result2 = it2.result ?: return@addOnCompleteListener
+                        for (time in result2) {
+                            val started_at = java.util.Date(time.data["started_at"].toString())
+                            val stopped_at = java.util.Date(time.data["stopped_at"].toString())
+//                            val duration = stopped_at - started_at
+                        }
+                    }
+                }
             }
         }
     }
